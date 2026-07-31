c95f1d8669c9f0aea758342ea049bdc2e4ad74ba
Fatih Ateş <fth20043@gmail.com>
2026-07-31 14:58:23 +0300

    Install required package, configure services for JWT.

--- Hotel_Management_Api/Hotel_Management_Api/obj/Debug/net10.0/Hotel_Management_Api.AssemblyInfo.cs
+++ Hotel_Management_Api/Hotel_Management_Api/obj/Debug/net10.0/Hotel_Management_Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotel_Management_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ef2efdc1fec35ccab4e21e0f445d74fdcd67419")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22bd895dcb06bab0dee491b6e20ad4a011a3bd3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotel_Management_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotel_Management_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
